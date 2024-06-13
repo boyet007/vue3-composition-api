4e6e598758b8be9623a967157293c738bdf60904
Philip Loa <boyet007@gmail.com>
2024-06-13 10:12:12 +0700

    laravel vue vite

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+// import router from './router/index.js';
+
+import App from './App.vue';
+// createApp(App).use(router).mount('#app');
+createApp(App).mount('#app');
